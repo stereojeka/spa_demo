66d8410977e95e8673970e4531c3b80aa37f7257
Jackson <enimenko@gmail.com>
2016-10-11 21:32:07 +0300

    config the satellizer

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $authProvider) {
 		name: 'facebook',
 		url: '/auth/facebook',
 		authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
-		redirectUri: window.location.origin + '/home',
+		redirectUri: window.location.origin + '/',
 		requiredUrlParams: ['display', 'scope'],
 		scope: ['email'],
 		scopeDelimiter: ',',
