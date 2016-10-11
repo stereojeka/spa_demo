258b5f8cc54b04b817ff4553a2bbce2d5f8e578f
Jackson <enimenko@gmail.com>
2016-10-11 20:35:03 +0300

    tuning resolve redirect

--- app/app.js
+++ app/app.js
@@ -1,26 +1,42 @@
 var app = angular.module('mainApp', ['ui.router']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: 'partials/home.tpl.html'
-    })
-    .state('secret', {
-      url: '/secret',
-      templateUrl: 'partials/secret.tpl.html',
-    })
-    .state('login', {
-      url: '/login',
-      templateUrl: 'partials/login.tpl.html'
-    });
-  $urlRouterProvider.otherwise('/home');
+	$stateProvider
+	.state('home', {
+		url: '/home',
+		resolve: {
+			"check": function($location, $rootScope){
+				if(!$rootScope.loggedIn){
+					$location.path('/');
+				}
+			}
+		},
+		templateUrl: 'partials/home.tpl.html'
+	})
+	.state('secret', {
+		url: '/secret',
+		resolve: {
+			"check": function($location, $rootScope){
+				if(!$rootScope.loggedIn){
+					$location.path('/');
+				} 
+			}
+		},
+		templateUrl: 'partials/secret.tpl.html'
+	})
+	.state('login', {
+		url: '/login',
+		templateUrl: 'partials/login.tpl.html'
+	});
+	$urlRouterProvider.otherwise('/login');
 
 });
 
-app.controller('loginController', function($scope, $location){
+app.controller('loginController', function($scope, $location, $rootScope){
 	$scope.submit = function(){
+
 		if($scope.username == 'admin' && $scope.password == 'admin'){
+			$rootScope.loggedIn = true;
 			$location.path('/home');
 		} else {
 			alert('Wrong stuff!');
