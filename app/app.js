3047b563f98ab83fa10e3015668a48508074363e
Jackson <enimenko@gmail.com>
2016-10-12 18:06:30 +0300

    C:/Program Files/Git

--- app/app.js
+++ app/app.js
@@ -170,10 +170,10 @@ app.controller('ProfileCtrl', function($scope, $auth, Account) {
 app.factory('Account', function($http) {
 	return {
 		getProfile: function() {
-			return $http.get('/api/me');
+			return $http.get('/spa_demo/api/me');
 		},
 		updateProfile: function(profileData) {
-			return $http.put('/api/me', profileData);
+			return $http.put('/spa_demo/api/me', profileData);
 		}
 	};
 });
