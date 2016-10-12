516bf49ecadb666fb61496e15bddbdb766f0ffeb
Jackson <enimenko@gmail.com>
2016-10-12 17:04:36 +0300

    try account.js

--- app/app.js
+++ app/app.js
@@ -167,3 +167,14 @@ app.controller('ProfileCtrl', function($scope, $auth, Account) {
 
 	$scope.getProfile();
 });
+
+app.factory('Account', function($http) {
+	return {
+		getProfile: function() {
+			return $http.get('/api/me');
+		},
+		updateProfile: function(profileData) {
+			return $http.put('/api/me', profileData);
+		}
+	};
+});
