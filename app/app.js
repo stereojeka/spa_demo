8aa28430151b8294e4a227bd10ea3ee59cbe0f4b
Jackson <enimenko@gmail.com>
2016-10-17 19:43:58 +0300

    go git auth

--- app/app.js
+++ app/app.js
@@ -65,15 +65,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $authProvider, $locatio
 
 	$authProvider.github({
 		clientId: '153ea8b8debdbfee9b1d',
-		responseType: 'token',
-		url: '/auth/github',
-		authorizationEndpoint: 'https://github.com/login/oauth/authorize',
-		redirectUri: window.location.origin + '/spa_demo/',
-		optionalUrlParams: ['scope'],
-		scope: ['user:email'],
-		scopeDelimiter: ' ',
-		oauthType: '2.0',
-		popupOptions: { width: 1020, height: 618 }
+		responseType: 'token'
 	});
 
 });
