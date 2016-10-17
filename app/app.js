a1941c2a3562d6fa9a8da92fac4dac0c4df28834
Jackson <enimenko@gmail.com>
2016-10-17 21:35:03 +0300

    fix git API

--- app/app.js
+++ app/app.js
@@ -56,12 +56,12 @@ app.config(function ($stateProvider, $urlRouterProvider, $authProvider, $locatio
 
 	$authProvider.github({
 		clientId: '153ea8b8debdbfee9b1d',
-		responseType: 'token',
+		//responseType: 'token',
 		url: '/auth/github',
 		authorizationEndpoint: 'https://github.com/login/oauth/authorize',
 		redirectUri: window.location.origin + '/spa_demo/',
 		optionalUrlParams: ['scope'],
-		scope: ['user:email'],
+		scope: ['user'],
 		scopeDelimiter: ' ',
 		oauthType: '2.0',
 		popupOptions: { width: 1020, height: 618 }
@@ -125,12 +125,9 @@ app.controller('logoutController', function($location, $localStorage) {
 
 app.controller('profileController', function($scope, $auth, Account, $localStorage) {
 
-	Account.getAuth()
-	.success(function() {
-		Account.getProfile()
-		.success(function(data) {
-			$scope.user = data;
-		});
+	Account.getProfile()
+	.success(function(data) {
+		$scope.user = data;
 	});
 
 /*
