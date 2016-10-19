227bee08c1d462709eed468ea2d4c363f76f557a
Jackson <enimenko@gmail.com>
2016-10-19 22:26:03 +0300

    add nutrionix api

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 var app = angular.module('mainApp', [
 	'ui.router',
- 	'satellizer',
-  	'ngResource',
-   	'ngStorage',
-   	'nix.api']);
+	'satellizer',
+	'ngResource',
+	'ngStorage',
+	'nix.api']);
 
 app.config(function ($stateProvider, $urlRouterProvider, $authProvider, $locationProvider, nixApiProvider) {
 	$stateProvider
@@ -177,5 +177,76 @@ app.factory('Account', function($http, $localStorage) {
 		getProfile: function() {
 			return $http.get('https://www.googleapis.com/plus/v1/people/me');
 		}
+	};
+});
+
+app.controller('foodController', function ($scope, $filter, nixApi) {
+	$scope.columns = [
+	{
+		header:     'qty',
+		valueField: 'serving_qty'
+	},
+	{
+		header:     'measure',
+		valueField: 'serving_unit'
+	},
+	{
+		header:     'food',
+		valueField: 'parsed_query.food'
+	},
+	{
+		header:     'Energy',
+		getValue:   function (estimation) {
+			var result;
+			if (estimation.nutrients) {
+				result = $filter('nutrient')(estimation.nutrients, 208);
+				if (result) {
+					return $filter('number')(result.value, 2).toString() + ' ' + result.unit;
+				}
+			}
+		},
+		getSummary: function () {
+			var result;
+			if ($scope.apiResponse && $scope.apiResponse.total) {
+				result = $filter('nutrient')($scope.apiResponse.total.nutrients, 208);
+				if (result) {
+					return $filter('number')(result.value, 2).toString() + ' ' + result.unit;
+				}
+			}
+		}
+	},
+	{
+		header:     'NDB number',
+		valueField: 'ndb_no'
+	}
+	];
+
+	$scope.data = '';
+	$scope.apiResponse = null;
+
+	$scope.estimate = function estimate() {
+		estimate.error = null;
+		$scope.apiResponse = null;
+		nixApi.natural($scope.data)
+		.success(function (apiResponse) {
+			$scope.apiResponse = apiResponse;
+		})
+		.error(function (error) {
+			estimate.error = error;
+		});
+	};
+
+	$scope.getProperty = function (object, property) {
+		var current = object, keys = property.split('.'), i;
+		for (i = 0; i < keys.length; i += 1) {
+			if (current[keys[i]]) {
+				current = current[keys[i]];
+			} else {
+				current = null;
+				break;
+			}
+		}
+
+		return current;
 	};
 });
