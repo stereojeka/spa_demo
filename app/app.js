caab00c879135658e466c748881556ebfbc5ffdb
Jackson <enimenko@gmail.com>
2016-10-20 17:06:36 +0300

    fix logic

--- app/app.js
+++ app/app.js
@@ -151,22 +151,33 @@ app.controller('logoutController', function($location, $localStorage) {
 app.controller('editProfileController', function($scope, $auth, Account, $localStorage) {
 
 	$scope.getProfile = function() {
-		Account.getProfile()
-		.then(function(response) {
-			$scope.user = response.data;
-			$localStorage.localdisplayName = $scope.user.displayName;
-			$localStorage.localtagline = $scope.user.displayName;
-			$localStorage.localimgUrl = $scope.user.image.url;
-		})
-		.catch(function(response) {
-			console.log(response.data.message, response.status);
-		});
+		if($localStorage.accessToken == null){
+			$localStorage.localdisplayName = $scope.userName;
+			$localStorage.localtagline = $scope.userStatus;
+		}else{
+			Account.getProfile()
+			.then(function(response) {
+				$scope.user = response.data;
+				$localStorage.localdisplayName = $scope.user.displayName;
+				$localStorage.localtagline = $scope.user.displayName;
+				$localStorage.localimgUrl = $scope.user.image.url;
+			})
+			.catch(function(response) {
+				console.log(response.data.message, response.status);
+			});
+		}
+
 	};
 
 	$scope.updateProfile = function() {
-		$scope.user.displayName = $localStorage.localdisplayName;
-		$scope.user.displayName = $localStorage.localtagline;
-		$scope.image.url = $localStorage.localimgUrl;
+		if($localStorage.accessToken == null){
+			$scope.userName = $localStorage.localdisplayName;
+			$scope.userStatus = $localStorage.localtagline;
+		}else{
+			$localStorage.localdisplayName = $scope.user.displayName;
+			$localStorage.localtagline = $scope.user.displayName;
+			$localStorage.localimgUrl = $scope.user.image.url;
+		}	
 	};
 
 	$scope.getProfile();
