276cd0cf5fbf48165b3271af75cb7cd522ed65e8
Jackson <enimenko@gmail.com>
2016-10-22 22:36:56 +0300

    try def

--- controllers/gmap.js
+++ controllers/gmap.js
@@ -1,5 +1,11 @@
 angular
 .module('mainApp')
 .controller('gmapController', function($scope) {
-	
+	var map;
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: -34.397, lng: 150.644},
+			zoom: 8
+		});
+	}
 });
