e76f209a3bf9bc1956119bb6328c4e0835c42a44
Jackson <enimenko@gmail.com>
2016-10-22 22:39:33 +0300

    remove code to html

--- controllers/gmap.js
+++ controllers/gmap.js
@@ -1,11 +1,5 @@
 angular
 .module('mainApp')
 .controller('gmapController', function($scope) {
-	var map;
-	function initMap() {
-		map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: -34.397, lng: 150.644},
-			zoom: 8
-		});
-	}
+	
 });
